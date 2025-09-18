908fbf92ef78d96d330d221ef7730c04e4f88bec
Diego Guevara <Prograjr3@pinulito.gt>
2025-09-18 09:15:41 -0600

    hotFix: handle error

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import 'dotenv/config'
 // import { config } from 'dotenv'
 import Routers from './routers/routers'
 import { connectionDb } from './config/database'
+import cors from 'cors'
+import errorHandlerMiddleware from "./middlewares/errorHandlerMiddleware";
 
 // config()
 
@@ -11,9 +13,15 @@ const app = express()
 
 const PORT = process.env.PORT || 8000
 
+app.use(cors())
+
 app.use(express.json())
 
+app.use(express.urlencoded({ extended: true }))
+
 app.use('/api', Routers)
+
+app.use(errorHandlerMiddleware)
 
 app.listen(PORT, ()=> {
 
